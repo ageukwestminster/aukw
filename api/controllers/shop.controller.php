66be5bacc9f14ac7a4dc07ef02aacf924a185e68
ncarthy <neil@shandonsoft.co.uk>
2021-05-01 23:21:24 +0100

    Add shop controller

---
+++ api/controllers/shop.controller.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controllers;
+
+class ShopCtl{
+
+
+  public static function read_all(){  
+
+    $model = new \Models\Shop();
+
+    echo json_encode($model->read(), JSON_NUMERIC_CHECK);
+  }
+
+
+  public static function read_one($id){  
+
+    $model = new \Models\Shop();
+    $model->id = $id;
+
+    echo json_encode($model->readone(), JSON_NUMERIC_CHECK);
+  }
+
+
+  public static function read_one_name($name){  
+
+    $model = new \Models\Shop();
+    $model->name = $name;
+
+    echo json_encode($model->readone(), JSON_NUMERIC_CHECK);
+  }
+
+}
