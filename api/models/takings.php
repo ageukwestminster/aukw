60fbef2ffacb7d19f9480b2b7c2f3ef5d1c8a97d
ncarthy <neil@shandonsoft.co.uk>
2021-05-01 23:42:06 +0100

    Add read one takings

---
+++ api/models/takings.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Models;
+
+use \PDO;
+
+class Takings{
+    // database conn 
+    private $conn;
+    // table name
+    private $table_name = "takings";
+
+    public function __construct(){
+        $this->conn = \Core\Database::getInstance()->conn;
+    }
+
+    // object properties
+    public $id;
+    public $date;
+    public $shopid;
+    public $clothing_num;
+    public $brica_num;
+    public $books_num;
+    public $linens_num;
+    public $donations_num;
+    public $other_num;
+    public $rag_num;
+    public $clothing;
+    public $brica;
+    public $books;
+    public $linens;
+    public $donations;
+    public $other;
+    public $rag;
+    public $customers_num_total;
+    public $cash_to_bank;
+    public $credit_cards;
+    public $operating_expenses;
+    public $volunteer_expenses;
+    public $other_adjustments;
+    public $cash_to_charity;
+    public $cash_difference;
+    public $comments;
+    public $rags_paid_in_cash;
+    public $timestamp;
+    public $quickbooks;
+
+    function readOne(){
+        $query = "SELECT
+                    takingsid, `date`, shopid, clothing_num, brica_num,
+                    books_num, linens_num, donations_num, other_num, rag_num, clothing,
+                    brica, books, linens, donations, other, rag, `customers_num_total`,
+                    cash_to_bank, `credit_cards`,`operating_expenses`,`volunteer_expenses`,
+                    `other_adjustments`, `cash_to_charity`, `cash_difference`,`comments`,
+                    `rags_paid_in_cash`,`timestamp`,quickbooks
+                    FROM
+                    " . $this->table_name . "
+                    WHERE takingsid = ?
+                    LIMIT 0,1";
+        
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );
+
+        // bind id of product to be updated
+        $stmt->bindParam(1, $this->id);
+
+        // execute query
+        $stmt->execute();
+
+        // get retrieved row
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // set values to object properties
+        $this->date = $row['date'];
+        $this->shopid = $row['shopid'];
+        $this->clothing_num = $row['clothing_num'];
+        $this->brica_num = $row['brica_num'];
+        $this->books_num = $row['books_num'];
+        $this->linens_num = $row['linens_num'];
+        $this->donations_num = $row['donations_num'];
+        $this->other_num = $row['other_num'];
+        $this->rag_num = $row['rag_num'];
+        $this->clothing = $row['clothing'];
+        $this->brica = $row['brica'];
+        $this->books = $row['books'];
+        $this->linens = $row['linens'];
+        $this->donations = $row['donations'];
+        $this->other = $row['other'];
+        $this->rag = $row['rag'];
+        $this->customers_num_total = $row['customers_num_total'];
+        $this->cash_to_bank = $row['cash_to_bank'];
+        $this->credit_cards = $row['credit_cards'];
+        $this->operating_expenses = $row['operating_expenses'];
+        $this->volunteer_expenses = $row['volunteer_expenses'];
+        $this->other_adjustments = $row['other_adjustments'];
+        $this->cash_to_charity = $row['cash_to_charity'];
+        $this->cash_difference = $row['cash_difference'];
+        $this->comments = $row['comments'];
+        $this->rags_paid_in_cash = $row['rags_paid_in_cash'];
+        $this->timestamp = $row['timestamp'];
+        $this->quickbooks = $row['quickbooks'];
+
+        // create array
+        $item = array(
+            "id" => $this->id,
+            "date" => $this->date,
+            "shopid" => $this->shopid,
+            "clothing_num" => $this->clothing_num,
+            "brica_num" => $this->brica_num,
+            "books_num" => $this->books_num,
+            "linens_num" => $this->linens_num,
+            "donations_num" => $this->donations_num,
+            "other_num" => $this->other_num,
+            "rag_num" => $this->rag_num,
+            "clothing" => $this->clothing,
+            "brica" => $this->brica,
+            "books" => $this->books,
+            "linens" => $this->linens,
+            "donations" => $this->donations,
+            "other" => $this->other,
+            "rag" => $this->rag,
+            "customers_num_total" => $this->customers_num_total,
+            "cash_to_bank" => $this->cash_to_bank,
+            "credit_cards" => $this->credit_cards,
+            "operating_expenses" => $this->operating_expenses,
+            "volunteer_expenses" => $this->volunteer_expenses,
+            "other_adjustments" => $this->other_adjustments,
+            "cash_to_charity" => $this->cash_to_charity,
+            "cash_difference" => $this->cash_difference,
+            "comments" => $this->comments,
+            "rags_paid_in_cash" => $this->rags_paid_in_cash,
+            "timestamp" => $this->timestamp,
+            "quickbooks" => $this->quickbooks 
+        );
+
+        return $item;
+    }
+
+}
