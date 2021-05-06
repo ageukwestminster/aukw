8b8592d4c2f56620d3e44c690c7c06bc22848a30
ncarthy <neil@shandonsoft.co.uk>
2021-05-07 00:45:06 +0100

    formatting

--- api/models/qbjournal.php
+++ api/models/qbjournal.php
@@ -158,140 +158,42 @@ class QuickbooksJournal{
         "TaxApplicableOn" => "Sales",
         "TaxAmount" => 0
       ]
+    ]);
+
+    $theResourceObj = JournalEntry::create($journal);
+    
+    $auth = new QuickbooksAuth();
+    $dataService = $auth->prepare();
+    $resultingObj = $dataService->Add($theResourceObj);
+
+    $error = $dataService->getLastError();
+    if ($error) {
+        echo "The Status code is: " . $error->getHttpStatusCode() . "\n";
+        echo "The Helper message is: " . $error->getOAuthHelperError() . "\n";
+        echo "The Response message is: " . $error->getResponseBody() . "\n";
+        return false;
+    } else {      
+      //echo json_encode(   $resultingObj );   
+      return array(
+          "id" => $resultingObj->Id,
+          "date" => $resultingObj->TxnDate,
+          "label" => $resultingObj->DocNumber
+      );
+    }
+  }
+
+  private function journal_line(&$line_array, $description, $amount, $account, $class) {
+    if (abs($amount) <= 0.005) return;
+
+    array_push($line_array, array(
+      "Description" => $description,
+      "Amount" => abs($amount),
+      "DetailType" => "JournalEntryLineDetail",
+      "JournalEntryLineDetail" => [
+        "PostingType" => $amount<=0?"Debit":"Credit",
+        "AccountRef" => $account,
+        "ClassRef" => $class
       ]
-    );
-
-/*
-        $obj = [
-            "TxnDate" => '2021-05-04',
-            "DocNumber" => "20210504H",
-            "Line" => [
-            [
-              "Description" => "Overage/Underage",
-              "Amount" => 0.2,
-              "DetailType" => "JournalEntryLineDetail",
-              "JournalEntryLineDetail" => [
-                "PostingType" => "Debit",
-                "AccountRef" => [
-                  "value" => 93,
-                  "name" => "Office Expense:Cash Discrepancies"
-                ],
-                "ClassRef" => [
-                  "value" => 400000000000618070,
-                  "name" => "Harrow Rd"
-                ]
-             ]
-            ],
-            [
-              "Description" => "Daily CC sales",
-              "Amount" => 302.6,
-              "DetailType" => "JournalEntryLineDetail",
-              "JournalEntryLineDetail" => [
-                "PostingType" => "Debit",
-                  "AccountRef" => [
-                    "value" => 96,
-                    "name" => "Credit Card Receipts"
-                  ],
-                  "ClassRef" => [
-                    "value" => 400000000000618070,
-                    "name" => "Harrow Rd"
-                  ]
-              ]
-            ],
-            [
-              "Description" => "Daily cash sales",
-              "Amount" => 226,
-              "DetailType" => "JournalEntryLineDetail",
-              "JournalEntryLineDetail" => [
-                "PostingType" => "Debit",
-                  "AccountRef" => [
-                    "value" => 100,
-                    "name" => "Undeposited Funds"
-                  ],
-                  "ClassRef" => [
-                    "value" => 400000000000618070,
-                    "name" => "Harrow Rd"
-                  ]
-              ]
-            ],
-            [
-              "Description" => "Zero-Rated Sales - Charity Shop Sales - Zero Rated",
-              "Amount" => 528.8,
-              "DetailType" => "JournalEntryLineDetail",
-              "JournalEntryLineDetail" => [
-                "PostingType" => "Credit",
-                "Entity" => [
-                  "Type" => "Vendor",
-                  "EntityRef" => [
-                    "value" => 33,
-                    "name" => "HMRC VAT"
-                  ]
-                ],
-                  "AccountRef" => [
-                    "value" => 94,
-                    "name" => "Sales-Zero Rated"
-                  ],
-                  "ClassRef" => [
-                    "value" => 400000000000618070,
-                    "name" => "Harrow Rd"
-                  ],
-                  "TaxCodeRef" => [
-                    "value" => 4
-                  ],
-                  "TaxApplicableOn" => "Sales",
-                  "TaxAmount" => 0
-              ]
-            ]
-          ],
-          "TxnTaxDetail"=> [
-            "TaxLine" => [
-                    "Amount" => 0,
-                    "DetailType" => "TaxLineDetail",
-                    "TaxLineDetail" => [
-                        "TaxRateRef" => [
-                            "value" => 7
-                        ],
-                        "PercentBased" => true,
-                        "TaxPercent" => 0,
-                        "NetAmountTaxable" => -528.8
-                    ]
-            ]
-          ]
-                      ];*/
-      $theResourceObj = JournalEntry::create($journal);
-      
-      $auth = new QuickbooksAuth();
-      $dataService = $auth->prepare();
-      $resultingObj = $dataService->Add($theResourceObj);
-
-      $error = $dataService->getLastError();
-      if ($error) {
-          echo "The Status code is: " . $error->getHttpStatusCode() . "\n";
-          echo "The Helper message is: " . $error->getOAuthHelperError() . "\n";
-          echo "The Response message is: " . $error->getResponseBody() . "\n";
-          return false;
-      } else {      
-        //echo json_encode(   $resultingObj );   
-        return array(
-            "id" => $resultingObj->Id,
-            "date" => $resultingObj->TxnDate,
-            "label" => $resultingObj->DocNumber
-        );
-      }
-    }
-
-    private function journal_line(&$line_array, $description, $amount, $account, $class) {
-      if (abs($amount) <= 0.005) return;
-
-      array_push($line_array, array(
-        "Description" => $description,
-        "Amount" => abs($amount),
-        "DetailType" => "JournalEntryLineDetail",
-        "JournalEntryLineDetail" => [
-          "PostingType" => $amount<=0?"Debit":"Credit",
-          "AccountRef" => $account,
-          "ClassRef" => $class
-       ]
-      ));
-    }
+    ));
+  }
 }
