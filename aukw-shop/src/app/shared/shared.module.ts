bb1c9c06d0e399199447dc58fa46cccaf1405db8
ncarthy <neil@shandonsoft.co.uk>
2021-09-15 23:52:22 +0100

    Login works

--- aukw-shop/src/app/shared/shared.module.ts
+++ aukw-shop/src/app/shared/shared.module.ts
@@ -3,20 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AlertComponent } from './alert-component/alert.component';
-import { ToastContainerComponent } from './toast-container/toast-container.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   declarations: [
     AlertComponent,
-    ToastContainerComponent,
   ],
   exports: [
     AlertComponent,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    ToastContainerComponent,
   ],
   providers: [ ],
 })
