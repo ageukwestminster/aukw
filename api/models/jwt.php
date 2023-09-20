9a07b3d3bdc4bde2f6400790c38b04b31899c6d4
ncarthy <neil@shandonsoft.co.uk>
2023-09-20 09:54:44 +0100

    Fix GUID method for windows

--- api/models/jwt.php
+++ api/models/jwt.php
@@ -365,11 +365,14 @@ class JWTWrapper{
     * for all supported pltforms with fallback to an older,
     * less secure version.
     *
-    * @param bool $trim remove leading and trailing spaces
+    * @param bool $trim If true then have no leading or trailing braces '{}'.
     * @return string The newly generated GUIDv4 string.
     */
     private function GUIDv4 ($trim = true)
     {
+        $lbrace = $trim ? "" : chr(123);    // "{"
+        $rbrace = $trim ? "" : chr(125);    // "}"
+
         // Windows
         if (function_exists('com_create_guid') === true) {
             if ($trim === true)
@@ -384,14 +387,20 @@ class JWTWrapper{
             $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // set version to 0100
             $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // set bits 6-7 to 10
             return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+            if ($trim === true)
+            {                   
+                return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+            }
+            else
+            {
+                return $lbrace.vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4)).$rbrace;
+            }
         }
 
         // Fallback (PHP 4.2+)
-        mt_srand((double)microtime() * 10000);
+        mt_srand((int)((double)microtime() * 10000));
         $charid = strtolower(md5(uniqid(rand(), true)));
         $hyphen = chr(45);                  // "-"
-        $lbrace = $trim ? "" : chr(123);    // "{"
-        $rbrace = $trim ? "" : chr(125);    // "}"
         $guidv4 = $lbrace.
                 substr($charid,  0,  8).$hyphen.
                 substr($charid,  8,  4).$hyphen.
