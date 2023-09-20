ac2b39facc04b3822709c573c6fe31837867c82b
ncarthy <neil@shandonsoft.co.uk>
2023-09-20 09:55:29 +0100

    Second try to fix

--- api/models/jwt.php
+++ api/models/jwt.php
@@ -386,15 +386,7 @@ class JWTWrapper{
             $data = openssl_random_pseudo_bytes(16);
             $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // set version to 0100
             $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // set bits 6-7 to 10
-            return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-            if ($trim === true)
-            {                   
-                return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-            }
-            else
-            {
-                return $lbrace.vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4)).$rbrace;
-            }
+            return $lbrace.vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4)).$rbrace;
         }
 
         // Fallback (PHP 4.2+)
