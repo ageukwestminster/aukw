d6a5a3173bd1bbc3ca98f614a77e723a0fe2b776
ncarthy <neil@shandonsoft.co.uk>
2023-09-20 17:49:36 +0100

    Add department sales class

--- api/index.php
+++ api/index.php
@@ -24,6 +24,7 @@ include_once 'controllers/takings.controller.php';
 include_once 'controllers/user.controller.php';
 
 // Define models
+include_once 'models/departmentsales.php';
 include_once 'models/jwt.php';
 include_once 'models/qbauth.php';
 include_once 'models/qbitem.php';
