ee27fdb6e7923005305d6ed0ae4dff67c8316a04
ncarthy <neil@shandonsoft.co.uk>
2024-04-18 22:26:54 +0100

    Replace include_once with require

--- api/index.php
+++ api/index.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This is the entry point for the api. It loads third party scripts from '/vendor/' and
+ * then requires all the php code files that make up the api. Finally it starts the 
+ * router so that when the user visits api endpoints they are directed to the correct place.
+ * 
+ * The structure of the app is:
+ * 1) Core php scripts that store constants, allow access to the database and provide helper functions
+ * 2) Controller classes that provide CRUD operations on the various models.
+ * 3) Model classes that retrieve/store data and contian business logic.
+ * 4) Routes.php and pre_routes.php which govern app routings.
+ * 5) Third party code in /vendor/
+ */
+
 // Load the composer autoloader
 require __DIR__ . '/vendor/autoload.php';
 
@@ -7,59 +20,59 @@ require __DIR__ . '/vendor/autoload.php';
 $router = new \Bramus\Router\Router();
 
 // Define core, database and helper classes
-include_once 'core/config.php';
-include_once 'core/database.php';
-include_once 'core/headers.php';
-include_once 'core/dateshelper.php';
-include_once 'core/qboconstants.php';
+require 'core/config.php';
+require 'core/database.php';
+require 'core/headers.php';
+require 'core/dateshelper.php';
+require 'core/qboconstants.php';
 
 // Define controllers
-include_once 'controllers/qbauth.controller.php';
-include_once 'controllers/qbbill.controller.php';
-include_once 'controllers/qbclass.controller.php';
-include_once 'controllers/qbcompany.controller.php';
-include_once 'controllers/qbemployee.controller.php';
-include_once 'controllers/qbitem.controller.php';
-include_once 'controllers/qbjournal.controller.php';
-include_once 'controllers/qbpayrolljournal.controller.php';
-include_once 'controllers/qbpayrollquery.controller.php';
-include_once 'controllers/qbrealm.controller.php';
-include_once 'controllers/qbrecurringtxn.controller.php';
-include_once 'controllers/qbreport.controller.php';
-include_once 'controllers/qbsalesreceipt.controller.php';
-include_once 'controllers/report.controller.php';
-include_once 'controllers/shop.controller.php';
-include_once 'controllers/takings.controller.php';
-include_once 'controllers/user.controller.php';
-include_once 'controllers/xlsx.controller.php';
+require 'controllers/qbauth.controller.php';
+require 'controllers/qbbill.controller.php';
+require 'controllers/qbclass.controller.php';
+require 'controllers/qbcompany.controller.php';
+require 'controllers/qbemployee.controller.php';
+require 'controllers/qbitem.controller.php';
+require 'controllers/qbjournal.controller.php';
+require 'controllers/qbpayrolljournal.controller.php';
+require 'controllers/qbpayrollquery.controller.php';
+require 'controllers/qbrealm.controller.php';
+require 'controllers/qbrecurringtxn.controller.php';
+require 'controllers/qbreport.controller.php';
+require 'controllers/qbsalesreceipt.controller.php';
+require 'controllers/report.controller.php';
+require 'controllers/shop.controller.php';
+require 'controllers/takings.controller.php';
+require 'controllers/user.controller.php';
+require 'controllers/xlsx.controller.php';
 
 // Define models
-include_once 'models/encryptedxlsx.php';
-include_once 'models/jwt.php';
-include_once 'models/payrollxlsx.php';
-include_once 'models/payslip.php';
-include_once 'models/qbauth.php';
-include_once 'models/qbbill.php';
-include_once 'models/qbclass.php';
-include_once 'models/qbemployee.php';
-include_once 'models/qbitem.php';
-include_once 'models/qbjournal.php';
-include_once 'models/qbnijournal.php'; // Depends on qbjournal.php
-include_once 'models/qbpayrolljournal.php'; // Depends on qbjournal.php
-include_once 'models/qbpensionbill.php'; // Depends on qbbill.php
-include_once 'models/qbquery.php';
-include_once 'models/qbsalesreceipt.php';
-include_once 'models/qbshopjournal.php'; // Depends on qbjournal.php
-include_once 'models/qbrealm.php';
-include_once 'models/qbrecurringtxn.php';
-include_once 'models/qbreport.php';
-include_once 'models/qbtoken.php';
-include_once 'models/report.php';
-include_once 'models/shop.php';
-include_once 'models/summary.php';
-include_once 'models/takings.php';
-include_once 'models/user.php';
-include_once 'models/usertoken.php';
+require 'models/encryptedxlsx.php';
+require 'models/jwt.php';
+require 'models/payrollxlsx.php';
+require 'models/payslip.php';
+require 'models/qbauth.php';
+require 'models/qbbill.php'; // This must be included before the files that depend on it.
+require 'models/qbclass.php';
+require 'models/qbemployee.php';
+require 'models/qbitem.php';
+require 'models/qbjournal.php'; // This must be included before the files that depend on it.
+require 'models/qbnijournal.php'; // Depends on qbjournal.php
+require 'models/qbpayrolljournal.php'; // Depends on qbjournal.php
+require 'models/qbpensionbill.php'; // Depends on qbbill.php
+require 'models/qbquery.php';
+require 'models/qbsalesreceipt.php';
+require 'models/qbshopjournal.php'; // Depends on qbjournal.php
+require 'models/qbrealm.php';
+require 'models/qbrecurringtxn.php';
+require 'models/qbreport.php';
+require 'models/qbtoken.php';
+require 'models/report.php';
+require 'models/shop.php';
+require 'models/summary.php';
+require 'models/takings.php';
+require 'models/user.php';
+require 'models/usertoken.php';
 
 // Define routes
 require 'pre_routes.php';
