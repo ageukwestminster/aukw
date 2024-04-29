e5fa2d22b2d179265eb634e1411e4082bb5b5e74
ncarthy <neil.carthy42@gmail.com>
2024-04-29 11:09:52 +0100

    Handle case of non-standard report

--- api/models/qbreport.php
+++ api/models/qbreport.php
@@ -169,15 +169,18 @@ class QuickbooksReport{
             echo "The Response message is: " . $error->getResponseBody() . "\n";
             return [];
         }
-        else {
-
-            //Now convert the report object to something readable
-
+        else {                        
             $report_arr=array();            
             
             /** @disregard Intelephense error on next line */
             $data = $report->Rows->Row[0]->Rows->Row;
 
+            // Handle case where report has multiple sections
+            if ($data[0] && property_exists($data[0], 'Rows')) {
+                $data = $data[0]->Rows->Row;
+            }
+
+            //Convert the report object to something readable
             foreach ($data as $value) {
                 $line=array();
                 
@@ -185,21 +188,42 @@ class QuickbooksReport{
 
                 $txn=array();
                 $txn['value'] = $value->ColData[1]->value;
-                if (isset($value->ColData[1]->id)) $txn['id'] = $value->ColData[1]->id;                
+                if (
+                    isset($value->ColData[1]->id) && 
+                    trim($value->ColData[1]->id) != ''
+                ) {
+                    $txn['id'] = $value->ColData[1]->id; 
+                } else {
+                    $txn['id'] = null;
+                }             
                 $line['type'] = $txn;
 
                 $line['docnumber'] = $value->ColData[2]->value;
 
                 $employee=array();
                 $employee['value'] = $value->ColData[3]->value;
-                if (isset($value->ColData[3]->id)) $employee['id'] = $value->ColData[3]->id;
+                if (
+                    isset($value->ColData[3]->id) && 
+                    trim($value->ColData[3]->id) != ''
+                ) {
+                    $employee['id'] = $value->ColData[3]->id; 
+                } else {
+                    $employee['id'] = null;
+                }
                 $line['emp_name'] = $employee;
 
                 $line['memo'] = $value->ColData[4]->value;
 
                 $account=array();
                 $account['value'] = $value->ColData[5]->value;
-                if (isset($value->ColData[5]->id)) $account['id'] = $value->ColData[5]->id; 
+                if (
+                    isset($value->ColData[5]->id) && 
+                    trim($value->ColData[5]->id) != ''
+                ) {
+                    $account['id'] = $value->ColData[5]->id; 
+                } else {
+                    $account['id'] = null;
+                }
                 $line['account'] = $account;
 
                 $line['is_cleared'] = $value->ColData[6]->value;
