495fba3aefdc0b2de78f9c6d25055e749a7eb9bb
Neil Carthy <neil.carthy@ageukwestminster.org.uk>
2024-12-08 22:33:49 +0000

    Comments

--- api/core/config.php
+++ api/core/config.php
@@ -48,22 +48,22 @@ class Config
 }
 
 // server location (for Access-Origin)
-Config::write('server', 'http://localhost:4200');
+Config::write('server', 'http://localhost:4200'); // Must change when deploying to production
 Config::write('api.path', '/api/');
 
 // db
-Config::write('db.host', '192.168.1.214');
-Config::write('db.port', '3306');
-Config::write('db.name', 'dailytakings');
-Config::write('db.user', 'shop');
-Config::write('db.password', 'DB_PASSWORD'); // env key name
+Config::write('db.host', '192.168.1.214');      // Database IP or hostname. Usually 'localhost' on produciton
+Config::write('db.port', '3306');               // standard MySql / MariaDB port
+Config::write('db.name', 'dailytakings');       // Database name
+Config::write('db.user', 'shop');               // Database user. All databse actions are performed by this single user.
+Config::write('db.password', 'DB_PASSWORD');    // Database user's password is stored as env variable
 
 // Note
 // Environment key values (such as 'DB_PASSWORD') are stored in:
 // Development: C:\Apache24\conf\httpd.conf
 // Production: ~/public_html/.htaccess (!! not ./api/.htaccess)
 
-// number of allowed password attempts
+// number of allowed password attempts. user is suspended if fails to login 6 times in a row
 Config::write('password_attempts', 5);
 
 // file upload settings
@@ -76,26 +76,26 @@ Config::write('token.accessExpiry', '+15 minute');
 Config::write('token.refreshExpiry', '+7 day');
 Config::write('token.iss', 'https://aukw.org.uk');
 Config::write('token.aud', 'https://aukw.org.uk');
-Config::write('token.envkeyname', 'AUKW_SHOP_KEY'); // env key name
+Config::write('token.envkeyname', 'AUKW_SHOP_KEY'); // environment variable name
 Config::write('token.cookiename', 'refreshToken');
 Config::write('token.cookiepath', Config::read('api.path') . 'auth');
 Config::write('token.cookiesecure', false);
 
-// Quickbooks SDKsettings
+// Quickbooks SDK settings
 Config::write('qb.authmode', 'oauth2');
 Config::write('qb.authrequesturi', 'https://appcenter.intuit.com/connect/oauth2');
 Config::write('qb.tokenendpointuri', 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer');
 Config::write('qb.authscope', 'com.intuit.quickbooks.accounting openid profile email');
 Config::write('qb.responsetype', 'code');
-Config::write('qb.authstate', 'QB_AUTH_STATE'); // used to verify QBO callback, env key name
+Config::write('qb.authstate', 'QB_AUTH_STATE'); // used to verify QBO callback, environment variable name
 Config::write('qb.enablelog', true);
 Config::write('qb.loglocation', 'B:\\logs'); 
 // Note: most logging goes into developer's %temp% directory
 // Only OAuth request and response end up in qb.loglocation
 
 Config::write('qb.baseUrl', 'Production');
-Config::write('qb.clientid', 'QB_CLIENT_ID'); // env key name
-Config::write('qb.clientsecret', 'QB_CLIENT_SECRET'); // env key name
+Config::write('qb.clientid', 'QB_CLIENT_ID'); // environment variable name
+Config::write('qb.clientsecret', 'QB_CLIENT_SECRET'); // environment variable name
 Config::write('qb.redirectdomain', 'https://e6c7-91-125-74-184.ngrok-free.app');
 Config::write('qb.redirecturl', Config::read('qb.redirectdomain') . '/callback');
 
