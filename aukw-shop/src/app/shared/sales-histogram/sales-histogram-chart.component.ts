17dd4fca3d53a653d769fb4e66367c99202f3c6f
Neil Carthy <neil.carthy@ageukwestminster.org.uk>
2025-03-02 16:01:33 +0000

    High data series in legend

--- aukw-shop/src/app/shared/sales-histogram/sales-histogram-chart.component.ts
+++ aukw-shop/src/app/shared/sales-histogram/sales-histogram-chart.component.ts
@@ -96,6 +96,7 @@ export class SalesHistogramChartComponent implements OnInit, OnChanges {
         marker: {
           radius: 1.5,
         },
+        showInLegend: false
       },
       {
         name: "Today's Sales",
