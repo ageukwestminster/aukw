16c5599328fc75987b60e3eb2dce260d5d456e8d
Neil Carthy <neil.carthy@ageukwestminster.org.uk>
2025-11-12 15:40:39 +0000

    Add barrel (index.ts) files to models sub directories

--- aukw-shop/src/app/_models/index.ts
+++ aukw-shop/src/app/_models/index.ts
@@ -13,32 +13,6 @@ export * from './takings';
 export * from './user';
 export * from './user-form-mode.enum';
 export * from './yes-no.enum';
-
-export * from './quickbooks/qb-account-list-entry';
-export * from './quickbooks/qb-attachment';
-export * from './quickbooks/qb-auth-uri';
-export * from './quickbooks/qb-connection-details';
-export * from './quickbooks/qb-flags';
-export * from './quickbooks/qb-payslip-flags';
-export * from './quickbooks/qb-purchase';
-export * from './quickbooks/qb-realm';
-export * from './quickbooks/qb-transfer';
-
-export * from './payroll/allocation';
-export * from './payroll/irispayslip';
-export * from './payroll/line-item-detail';
-export * from './payroll/employee';
-export * from './payroll/payroll-journal-entry';
-export * from './payroll/payrun';
-export * from './payroll/taxyear';
-
-export * from './reports/avg-daily-txn-data';
-export * from './reports/avg-weekly-sales-data';
-export * from './reports/chart_data';
-export * from './reports/profit-and-loss-data';
-export * from './reports/qma-report';
-export * from './reports/ragging-quarter';
-export * from './reports/sales-by-dept';
-export * from './reports/sales-by-item';
-export * from './reports/summary';
-export * from './reports/takings-summary';
+export * from './quickbooks';
+export * from './payroll';
+export * from './reports';
---
+++ aukw-shop/src/app/_models/payroll/index.ts
@@ -0,0 +1,7 @@
+export * from './allocation';
+export * from './irispayslip';
+export * from './line-item-detail';
+export * from './employee';
+export * from './payroll-journal-entry';
+export * from './payrun';
+export * from './taxyear';
